500c8170607774b6219be5d30ab07540eb1b62d1
NinoMaj <nino.majder@gmail.com>
2017-07-12 21:34:16 +0200

    add item image

---
+++ src/shared/component/item-image.jsx
@@ -0,0 +1,54 @@
+// @flow
+
+import React from 'react'
+import styled from 'styled-components'
+
+const ImgContainer = styled.div`
+  position: relative;
+`
+const Img = styled.img`
+  height: ${() => ((Math.random() * (300 - 150)) + 150)}px;
+  width: 226px;
+  margin: 5px auto;
+  display: block;
+
+  ${ImgContainer}:hover & {
+    filter: brightness(50%);
+    transition: all 1s ease;
+  }
+`
+const PinButton = styled.div`
+  text-align: center;
+  width: 30%;
+  height: 30px;
+  color: white;
+  background-color: rgb(180, 11, 31);
+  border-radius: 4px;
+  position: absolute;
+  top: 5px;
+  right: 10px;
+  display: none;
+  cursor: pointer;
+
+  ${ImgContainer}:hover & {
+	  display: block;
+	}
+`
+const PinText = styled.p`
+  padding-top: 3px;
+`
+
+type Props ={
+  source: string,
+}
+
+const ItemImage = ({ source}: Props) => (
+  <ImgContainer>
+    <Img src={source} alt="pin" />
+    <PinButton>
+      <PinText>PIN <i className="fa fa-thumb-tack" aria-hidden="true" /></PinText>
+    </PinButton>
+  </ImgContainer>
+)
+
+export default ItemImage
--- src/shared/component/item.js
+++ src/shared/component/item.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import ItemImage from './item-image'
+
 const ItemContainer = styled.div`
   background-color: lightgray;
   float: left;
@@ -8,12 +10,7 @@ const ItemContainer = styled.div`
   margin-bottom: 10px;
   box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2), 0 4px 20px 0 rgba(0,0,0,0.19);
 `
-const Img = styled.img`
-  height: ${() => ((Math.random() * (300 - 150)) + 150)}px;
-  width: 226px;
-  margin: 5px auto;
-  display: block;
-`
+
 const ItemInfo = styled.div`
   text-align: center;
   display: flex;
@@ -27,11 +24,10 @@ const ItemText = styled.p`
 
 const Item = () => (
   <ItemContainer className="grid-item" >
-    <Img src={'https://unsplash.it/200/200?image=#{$id}'} alt="card" />
+    <ItemImage source={'https://source.unsplash.com/random/326x300'} />
     <ItemInfo>
       <ItemText>Lorem Ipsum something something...</ItemText>
     </ItemInfo>
   </ItemContainer>
 )
-
 export default Item
