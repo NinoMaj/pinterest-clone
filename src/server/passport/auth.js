05b98c74a67ebb1180ce02fb8b0c55c1df92fde3
RamiroPinol <ramirop@protonmail.com>
2017-08-01 00:59:44 -0300

    Changes to deploy

--- src/server/passport/auth.js
+++ src/server/passport/auth.js
@@ -1,16 +1,20 @@
+const callbackPath = process.env.NODE_ENV === 'production' ?
+  'https://chingu-showcase.herokuapp.com/' :
+  'http://localhost:8000/'
+
 module.exports = {
   twitterAuth: {
     consumerKey: '1OfRIUy70ChhM0zwhd59oDGsO',
-    callbackURL: 'http://localhost:8000/auth/twitter/callback',
+    callbackURL: `${callbackPath}auth/twitter/callback`,
   },
 
   googleAuth: {
     clientID: '566331442205-7uip1rjq3kb12kcjd3r458qbjbv3tri4.apps.googleusercontent.com',
-    callbackURL: 'http://localhost:8000/auth/google/callback',
+    callbackURL: `${callbackPath}auth/google/callback`,
   },
 
   githubAuth: {
     clientID: 'c44d960c8b1e4fa06dbf',
-    callbackURL: 'http://localhost:8000/auth/github/callback',
+    callbackURL: `${callbackPath}auth/github/callback`,
   },
 }
