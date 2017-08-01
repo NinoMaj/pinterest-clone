72c1819be12f8a93dce2d8ff423cc0167147e9f0
RamiroPinol <ramirop@protonmail.com>
2017-08-01 17:06:00 -0300

    Added separated auths for production and development

--- src/server/passport/auth.js
+++ src/server/passport/auth.js
@@ -1,10 +1,13 @@
-const callbackPath = process.env.NODE_ENV === 'production' ?
+const isProduction = process.env.NODE_ENV === 'production'
+console.log(isProduction)
+
+const callbackPath = isProduction ?
   'https://chingu-showcase.herokuapp.com/' :
   'http://localhost:8000/'
 
 module.exports = {
   twitterAuth: {
-    consumerKey: '1OfRIUy70ChhM0zwhd59oDGsO',
+    consumerKey: isProduction ? '1OfRIUy70ChhM0zwhd59oDGsO' : 'pDgW2qdfYmE7siYxboN5esjsX',
     callbackURL: `${callbackPath}auth/twitter/callback`,
   },
 
@@ -14,7 +17,7 @@ module.exports = {
   },
 
   githubAuth: {
-    clientID: 'c44d960c8b1e4fa06dbf',
+    clientID: isProduction ? 'c44d960c8b1e4fa06dbf' : '268f2bef895718508c27',
     callbackURL: `${callbackPath}auth/github/callback`,
   },
 }
