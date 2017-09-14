8102e13dfdfd3bfeba2297bbfde10f25df268951
RamiroPinol <ramirop@protonmail.com>
2017-09-14 17:09:56 -0300

    Profile API error handling and public profile page improvements

--- src/shared/component/page/user-page.jsx
+++ src/shared/component/page/user-page.jsx
@@ -35,18 +35,26 @@ const ProjectsTitle = styled.div`
   margin: 25px auto 10px;
 `
 
+const ErrorMsg = styled.div`
+  font-size: 32px;
+  text-align: center;
+`
+
 class UserPage extends Component {
   constructor(props) {
     super(props)
     this.state = {
       user: {},
+      error: '',
     }
   }
 
   componentDidMount() {
     fetch(GET_USER_ROUTE(this.props.showUser))
       .then(res => res.json())
-      .then(json => this.setState({ user: json }))
+      .then(json =>
+        json.error ? this.setState({ error: json.error }) : this.setState({ user: json }),
+      )
   }
 
   // Function to build location string depending on which of city, state and country
@@ -61,7 +69,7 @@ class UserPage extends Component {
 
   render() {
     const { userName, fullName } = this.state.user
-    const title = `${userName}'s Profile`
+    const title = this.state.error ? 'User not found' : `${userName}'s Profile`
     return (
       <div className="container mt-4">
         <Helmet
@@ -74,17 +82,22 @@ class UserPage extends Component {
         <div className="row">
           <div className="col-12">
 
-            <UserInfoDiv className="userInfo">
-              <Img src="/static/images/chingu-logo.png" alt="user avatar" />
-              <Name><b>{userName}</b> {fullName ? `(${fullName})` : ''}</Name>
-              <LocationDiv>
-                {this.setLocation()}
-              </LocationDiv>
-            </UserInfoDiv>
+            { this.state.error ?
+              <ErrorMsg>{`Ooops! ${this.state.error}`}</ErrorMsg> :
+              <div>
+                <UserInfoDiv className="userInfo">
+                  <Img src="/static/images/chingu-logo.png" alt="user avatar" />
+                  <Name><b>{userName}</b> {fullName ? `(${fullName})` : ''}</Name>
+                  <LocationDiv>
+                    {this.setLocation()}
+                  </LocationDiv>
+                </UserInfoDiv>
 
-            <LoginModal />
-            <ProjectsTitle>Projects</ProjectsTitle>
-            <Gallery page={USER_PAGE_ROUTE} profileUserName={this.props.showUser} />
+                <LoginModal />
+                <ProjectsTitle>Projects</ProjectsTitle>
+                <Gallery page={USER_PAGE_ROUTE} profileUserName={this.props.showUser} />
+              </div>
+            }
 
           </div>
         </div>
